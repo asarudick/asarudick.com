d6069092987b7dfae7e3b23da874c8807b21bca4
Shabonkerz <Shabonkerz@gmail.com>
2016-04-26 20:09:36 -0500

    add syntax highlighting to blog, and further separate styles

--- app/routes/blog.js
+++ app/routes/blog.js
@@ -2,4 +2,6 @@ import Ember from 'ember';
 import TransitionRoute from '../mixins/transition-route';
 
 export default Ember.Route.extend(TransitionRoute, {
+	didTransition () {
+	}
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -26,6 +26,7 @@ module.exports = function (defaults) {
     // modules that you would like to import into your application
     // please specify an object with the list of modules as keys
     // along with the exports of each module as its value.
+
 	app.import('bower_components/remarkable/dist/remarkable.min.js');
 	app.import('bower_components/slideout.js/dist/slideout.min.js');
 	app.import('bower_components/lodash/lodash.js');
