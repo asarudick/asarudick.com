236ca6d398efe6337349eb74c7287bfd7a9ce533
Shabonkerz <Shabonkerz@gmail.com>
2016-04-27 10:08:06 -0500

    add header to blog

--- app/router.js
+++ app/router.js
@@ -2,13 +2,15 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
-Router.map(function() {
-  this.route('blog');
-  this.route('projects');
-  this.route('contact');
+Router.map(function () {
+    this.route('blog', function () {
+        this.route('trading-memory-for-performance-in-js-engines');
+    });
+    this.route('projects');
+    this.route('contact');
 });
 
 export default Router;
